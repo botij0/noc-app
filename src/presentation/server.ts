2888fe1520501957ecee79dbd826d18eea602a2c
botij0 <javier.gonzalez.soldado@gmail.com>
2026-01-18 13:59:48 +0100

    feat: logrepositoryimpl

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,9 @@
-import { CheckService } from "../domain/user-cases/checks/check-service";
 import { CronService } from "./cron/cron-service";
+import { CheckService } from "../domain/user-cases/checks/check-service";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.imp";
+import { FileSystemDatasource } from "../infrastructure/datasources/File-system.datasource";
+
+const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
 
 export class Server {
   public static start() {
@@ -8,6 +12,7 @@ export class Server {
     CronService.createJob("*/5 * * * * *", () => {
       const url = "https://google.com";
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`${url} is ok`),
         (error) => console.log(error),
       ).execute(url);
